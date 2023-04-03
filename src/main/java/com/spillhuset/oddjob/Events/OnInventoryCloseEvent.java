0501f75ca325858e297838b2602ed5fca8fda0f2
odd-e <jovang@gmail.com>
2023-04-03 07:35:08 +0200

    .

--- src/main/java/com/spillhuset/oddjob/Events/OnInventoryCloseEvent.java
+++ src/main/java/com/spillhuset/oddjob/Events/OnInventoryCloseEvent.java
@@ -1,9 +1,11 @@
 package com.spillhuset.oddjob.Events;
 
+import com.spillhuset.oddjob.Enums.PriceList;
 import com.spillhuset.oddjob.OddJob;
 import org.bukkit.GameMode;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -12,9 +14,43 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public class OnInventoryCloseEvent implements Listener {
-    @EventHandler (priority = EventPriority.HIGHEST)
-    public void onInventoryMove(InventoryInteractEvent event ) {
+    @EventHandler
+    public void onInventoryInteract(InventoryInteractEvent event) {
+        HumanEntity clicker = event.getWhoClicked();
+        Inventory inventory = event.getInventory();
+        if (event.getView().getTitle().equalsIgnoreCase("COMMON SHOP")) {
+            event.setResult(Event.Result.DENY);
+            ItemStack itemStack = clicker.getItemOnCursor();
+            event.
+            PriceList.valueOf()
+        }
 
+        OddJob.getInstance().log("inventory interact event");
+    }
+
+    @EventHandler
+    public void onInventoryMove(InventoryMoveItemEvent event) {
+        OddJob.getInstance().log("inventory move item event");
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        OddJob.getInstance().log("inventory click event");
+    }
+
+    @EventHandler
+    public void onInventoryPickup(InventoryPickupItemEvent event) {
+        OddJob.getInstance().log("inventory pickup item event");
+    }
+
+    @EventHandler
+    public void onInventoryDrag(InventoryDragEvent event) {
+        OddJob.getInstance().log("inventory drag event");
+    }
+
+    @EventHandler
+    public void onInventory(InventoryEvent event) {
+        OddJob.getInstance().log("inventory event");
     }
 
     @EventHandler
@@ -33,6 +69,6 @@ public class OnInventoryCloseEvent implements Listener {
                 inventory.clear();
             }
         }
-        OddJob.getInstance().getPlayerManager().removeInventory(inventory,player);
+        OddJob.getInstance().getPlayerManager().removeInventory(inventory, player);
     }
 }
